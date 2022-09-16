04d1b5601183106b61df89dd2f238d694cad061b
Vinicius de Borba <vinicius_deborba@hotmail.com>
2022-09-16 19:02:26 -0300

    Start login page

---
+++ todo-list-app/src/features/users/login/login-card.component.js
@@ -0,0 +1,11 @@
+import logo from '../../../logo.svg';
+
+export const LoginCard = () => {
+    return (
+        <div className="Login-card">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      );
+};
+
+
---
+++ todo-list-app/src/pages/login.js
@@ -0,0 +1,12 @@
+import { LoginCard } from '../features/users/login/login-card.component';
+import '../styles/Login.css'
+
+export const Login = () => {
+    return (
+        <div className="Login">
+            <LoginCard />
+        </div>
+      );
+};
+
+
