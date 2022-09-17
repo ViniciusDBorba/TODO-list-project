bcd67198d642581efe5db7fd11327b0f60750ed5
Vinicius de Borba <vinicius_deborba@hotmail.com>
2022-09-17 00:53:28 -0300

    Add router

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,11 +1,24 @@
 import {Login} from './pages/login'
 import { Register } from './pages/register';
+import { ProjectList } from './pages/project-list';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />            
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<ProjectList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ todo-list-app/src/pages/project-list.js
@@ -0,0 +1,9 @@
+
+export const ProjectList = () => {
+    return (
+        <div className="Project-list">
+        </div>
+      );
+};
+
+
